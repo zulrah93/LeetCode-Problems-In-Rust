ce98caefa26a07f4aad8fa85ff945c6ed721bf16
zulrah <dlope073@gmail.com>
2021-04-17 16:15:43 -0400

    Add solution #70

---
+++ 70.climbing-stairs.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut cache = HashMap::new();
+        climb_stairs_with_cache(n, &mut cache)
+    }
+}
+
+fn climb_stairs_with_cache(n : i32, cache : &mut HashMap<i32,i32>) -> i32 {
+    
+    
+    if n == 1 {
+        1
+    }    
+    else if n == 2 {
+        2
+    }
+    else if cache.contains_key(&n) {
+        *cache.get(&n).unwrap()
+    }
+    else {
+        
+        let x = climb_stairs_with_cache(n-1, cache);
+        let y = climb_stairs_with_cache(n-2, cache);
+        
+        cache.insert(n-1, x);
+        cache.insert(n-2, y);
+        
+        x+y
+    }
+    
+    
+}
