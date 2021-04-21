22e44277af88ef2a245b27ee0c418c26bdb67e23
dlopez <dlopez@farelogix.com>
2021-04-21 10:05:02 -0400

    Add solution #33

---
+++ 33.search-in-rotated-sorted-array.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        
+        for (i, v) in nums.iter().enumerate() {
+            if *v == target {
+                return i as i32;
+            }
+        }
+        
+        -1
+    }
+}
