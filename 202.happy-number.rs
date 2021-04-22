e25403f78fcb0e60a5bb748e6223436ecd8d9845
dlopez <dlopez@farelogix.com>
2021-04-22 13:51:56 -0400

    Add solution #202

---
+++ 202.happy-number.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        
+        let mut set = HashSet::new();
+        let mut x = n;
+        
+        while x != 1 {
+            
+            let mut y = 0;
+            
+            while x > 0 {
+                y += (x % 10).pow(2) as i32;
+                x /= 10;
+            }
+            
+            if set.contains(&y) {
+                return false;
+            }
+            else {
+                set.insert(y);
+            }
+            
+            x = y;
+        }
+        
+        true
+        
+    }
+}
