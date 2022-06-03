b9bb44a79bf5da04902ad82e091998c8ff3cf277
zulrah <1s16slrse@mozmail.com>
2022-06-02 22:06:31 -0400

    Run rustfmt on the new leetcode solution

--- 448.find.all.numbers.disappeared.in.an.array.rs
+++ 448.find.all.numbers.disappeared.in.an.array.rs
@@ -4,6 +4,8 @@ impl Solution {
         let mut sorted_nums = nums.clone(); // Space complexity O(N)
         sorted_nums.sort();
         // Using iterators and higher order functions -- can be done using loops but lines are precious real estate 🛖🏚️🏠🏡🏘️
-         (1..((nums.len() as i32)+1)).filter(|x| sorted_nums.binary_search(x).is_err()).collect()
+        (1..((nums.len() as i32) + 1))
+            .filter(|x| sorted_nums.binary_search(x).is_err())
+            .collect()
     }
 }
