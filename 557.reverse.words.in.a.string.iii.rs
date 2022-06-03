3854f60560d797ccdd264c9972249406be1250f2
zulrah <1s16slrse@mozmail.com>
2022-06-02 22:15:30 -0400

    Run rustfmt and add comments to solution #557

---
+++ 557.reverse.words.in.a.string.iii.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        let mut result = String::default();
+        for token in s
+            .split(' ') // Split string based off whitespace character
+            .map(|x| x.to_string().chars().rev().collect::<String>()) // Map each split string with the reverse
+        {
+            result.push_str(token.as_str());
+            result.push_str(" "); // We will trim the string at the end 
+        }
+        result.trim().to_string()
+    }
+}
