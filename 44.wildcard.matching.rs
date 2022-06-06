6c5357d6d6d9fb7faf61ee87459ba899ec89f76d
zulrah <1s16slrse@mozmail.com>
2022-06-06 15:03:54 -0400

    [Work In Progress] Refactor and pass 9/10 LeetCode cases and add one custom test case

--- 44.wildcard.matching.rs
+++ 44.wildcard.matching.rs
@@ -1,8 +1,18 @@
+fn pattern_has_no_wildcard(pattern: &String) -> bool {
+    pattern
+        .chars()
+        .filter(|c| get_wildcard_type(c) == WildCard::None) // Only keep the none special characters
+        .collect::<String>()
+        .eq(pattern) // Check if it matches with the original pattern and if true the pattern has no special characters
+}
+
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
-        if s == p {
-            // Case where the pattern equals the input string
-            true
+        println!("Input: {} Pattern: {}", s, p);
+        let no_wildcards = pattern_has_no_wildcard(&p);
+        if no_wildcards {
+            // No wildcards
+            s == p
         } else if p == "*".to_string() {
             // Case where the pattern is just a kleene star (Accept any input string)
             true
@@ -34,11 +44,6 @@ fn is_match_dfs(
     pattern: &Vec<char>,
     pattern_index: usize,
 ) -> bool {
-    print!(
-        "input_index = {} pattern_index = {} ",
-        input_index, pattern_index,
-    );
-
     if input_index < input.len() {
         if pattern_index >= pattern.len() {
             // We have an input greater than the pattern given for example [ input: "a"  pattern: "aa" ]
@@ -50,15 +55,12 @@ fn is_match_dfs(
 
         let input_pattern_match = current_input == current_pattern;
         let wildcard_type = get_wildcard_type(current_pattern); //
-        let pattern_at_end = if pattern_index == 0 {
-            false
-        } else {
-            (pattern_index + 1) == pattern.len()
-        }; // Denotes that the index is at the final position
+        let pattern_at_end = (pattern_index + 1) == pattern.len(); // Kleene star is at the end [special case]
+        let pattern_at_start = pattern_index == 0; // Kleene star is at the start  [special case]
 
         println!(
-            " wildcard_type = {:?} current_pattern = {} current_input = {}",
-            wildcard_type, current_pattern, current_input
+            "input[{}] = {} pattern[{}] = {}",
+            input_index, current_input, pattern_index, current_pattern
         );
 
         match wildcard_type {
@@ -76,6 +78,28 @@ fn is_match_dfs(
                 if pattern_at_end {
                     // If the ending pattern is a kleene star our job is done because we can match anything including an empty string
                     true
+                } else if pattern_at_start {
+                    // Note: we already checked for '*' before the helper method was called
+
+                    let next_pattern = &pattern[pattern_index + 1];
+                    let mut index = input_index;
+                    let mut passed = false; // If set to true
+
+                    while index < input.len() {
+                        let current = &input[index];
+                        if current == next_pattern {
+                            passed = true;
+                            break;
+                        }
+                        index += 1;
+                    }
+
+                    if passed {
+                        // If we patch we move the input index by one to advance to the next set of symbols
+                        is_match_dfs(input, index + 1, pattern, pattern_index + 2)
+                    } else {
+                        false
+                    }
                 } else {
                     let next_pattern = &pattern[pattern_index + 1];
                     let input_pattern_match = next_pattern == current_input; // Note: This overrides the local variable outside of the match scope; so we keep consistency in the variable names
@@ -90,7 +114,7 @@ fn is_match_dfs(
             }
         }
     } else {
-        pattern_index == pattern.len() // Returns true if we fully matched with the pattern if not a partial will return false
+        true
     }
 }
 
@@ -138,4 +162,32 @@ fn case_6() {
 #[test]
 fn case_7() {
     assert_eq!(Solution::is_match("ab".to_string(), "?*".to_string()), true);
+}
+
+#[test]
+fn case_8() {
+    assert_eq!(Solution::is_match("aa".to_string(), "a".to_string()), false);
+}
+
+#[test]
+fn case_9() {
+    assert_eq!(Solution::is_match("a".to_string(), "aa".to_string()), false);
+}
+
+#[test]
+fn case_10() {
+    assert_eq!(
+        Solution::is_match("abcabczzzde".to_string(), "*abc???de*".to_string()),
+        false
+    );
+}
+
+// Custom input case
+
+#[test]
+fn custom_case_1() {
+    assert_eq!(
+        Solution::is_match("ddradceb".to_string(), "*a*b".to_string()),
+        true
+    );
 }
