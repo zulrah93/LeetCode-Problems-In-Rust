10a469324afb4c91a61dd246d75d258df13dbf65
zulrah <1s16slrse@mozmail.com>
2022-06-16 13:50:37 -0400

    Complete correctly Solution #5 using non DP approach

--- src/problems/5.longest.palindromic.substring.rs
+++ src/problems/5.longest.palindromic.substring.rs
@@ -1,59 +1,69 @@
-/*
-    WORK IN PROGRESS [⚠️ Incomplete Solution ⚠️]
-*/
+use std::cmp::max;
 
-fn is_even_length(string: &String) -> bool {
-    string.len() % 2 == 0
+fn get_tuple_length(tuple: &(usize, usize)) -> usize {
+    (tuple.1 - tuple.0) + 1
+}
+
+fn expand_around_center(
+    string_vec: &Vec<char>,
+    left_index: usize,
+    right_index: usize,
+) -> (usize, usize) {
+    let length = string_vec.len();
+    let mut left_index = left_index as i64;
+    let mut right_index = right_index;
+
+    while left_index >= 0 && right_index < length {
+        // Worst case complexity: O(N) with constant space
+
+        let left_char = &string_vec[left_index as usize];
+        let right_char = &string_vec[right_index];
+        if left_char != right_char {
+            // Note: Rust doesn't compare by reference so this is considered safe unlike Java for example
+            break; // We break out of loop since we no longer have a palindrome
+        }
+        left_index -= 1;
+        right_index += 1;
+    }
+
+    let left_index = (left_index + 1) as usize;
+    let right_index = right_index;
+
+    let result = (left_index, right_index); // The tuple returned contains the starting index and ending index (+1) of the largest palindrome
+
+    result
 }
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        let length = s.len(); // Used to determine mid point of string
-        let chars = s.chars().collect::<Vec<char>>();
+        //println!("Finding Longest Palindrome For: [{}]", s);
 
+        if s.is_empty() {
+            return String::default();
+        }
+
+        let length = s.len();
+        let string_vec = s.chars().collect::<Vec<char>>();
         let mut start_index = 0usize;
-        let mut end_index = 1usize; // We default to 1 so we can at least return a single character which by definition is the smallest palidrone
+        let mut end_index = 1usize;
 
-        if is_even_length(&s) {
-            // Case: Even length string which is like odd case
+        for index in 0..length {
+            let odd_tuple = expand_around_center(&string_vec, index, index);
+            let even_tuple = expand_around_center(&string_vec, index, index + 1); // Even length means they are two centers or visually an empty character in between the two center characters
 
-            let midpoint_index_a = (length / 2) - 1; // We are going to start at the midpoint
-            let midpoint_index_b = midpoint_index_a + 1; // Even length strings has a secondary midpoint
+            let max_tuple = if get_tuple_length(&odd_tuple) >= get_tuple_length(&even_tuple) {
+                odd_tuple
+            } else {
+                even_tuple
+            };
 
-            for offset in 0..(length / 2) {
-                let current_left_char = &chars[midpoint_index_a - offset]; // Looking at the left most character from midpoint character
-                let current_right_char = &chars[midpoint_index_b + offset]; // Looking at the right most character from midpoint character
-
-                if current_left_char != current_right_char {
-                    // Note: Rust never compares references like Java for example so this code is safe
-                    break; // We no longer have a palindrome and we essentially no longer update start and end index which will be the bounds of the longest palidromic substring
-                }
-
-                start_index = midpoint_index_a - offset;
-                end_index = (midpoint_index_b + offset) + 1;
-            }
-        } else {
-            // Case: Odd length string
-
-            let midpoint_index = length / 2; // We are going to start at the midpoint
-
-            for offset in 0..((length / 2) + 1) {
-                // O(N/2) where N is |s|
-
-                let current_left_char = &chars[midpoint_index - offset]; // Looking at the left most character from midpoint character
-                let current_right_char = &chars[midpoint_index + offset]; // Looking at the right most character from midpoint character
-
-                if current_left_char != current_right_char {
-                    // Note: Rust never compares references like Java for example so this code is safe
-                    break; // We no longer have a palindrome and we essentially no longer update start and end index which will be the bounds of the longest palidromic substring
-                }
-
-                start_index = midpoint_index - offset;
-                end_index = (midpoint_index + offset) + 1;
+            if get_tuple_length(&max_tuple) >= get_tuple_length(&(start_index, end_index)) {
+                start_index = max_tuple.0;
+                end_index = max_tuple.1;
             }
         }
 
-        chars[start_index..end_index].iter().collect::<String>()
+        string_vec[start_index..end_index].iter().collect()
     }
 }
 
@@ -62,6 +72,14 @@ fn odd_length_test_case_1() {
     assert_eq!(
         "aba".to_string(),
         Solution::longest_palindrome("babad".to_string())
+    );
+}
+
+#[test]
+fn odd_length_test_case_2() {
+    assert_eq!(
+        "bb".to_string(),
+        Solution::longest_palindrome("abb".to_string())
     );
 }
 
